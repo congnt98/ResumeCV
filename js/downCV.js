ce5ac781d20bec052101202570e2e347f6ab87bf
congnt98 <ntcong.exe@gmail.com>
2024-08-02 16:53:44 +0700

    Update animate

---
+++ js/downCV.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadWord').addEventListener('click', function(e) {
+    console.log('CV Word file is being downloaded');
+    // Bạn có thể thêm code ở đây để theo dõi số lần tải xuống hoặc thực hiện các hành động khác
+  });
